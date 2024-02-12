805ae68b065e457c0d61a3c5200f618223d7811b
pankajra1 <pankajrai_21911@aitpune.edu.in>
2024-02-12 11:57:03 +0530

    updating app.jsx and adding new files

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,17 @@ import {
 } from "react-router-dom";
 // import SignUp from './components/SignUp'
 import './App.css'
+
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import Home from './components/Home';
+import Footer from './components/Footer';
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Login />} />
-                <Route path='/signup' element={<SignUp />} />
-            </Routes>
-        </Router>
+      <>
+      <Home/>
+     <Footer/>
+      </>
     )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,54 @@
+import React from "react"; 
+import logo from "/src/assets/logo.png";
+const Home = () => {
+  return (
+    <div>
+      {/* Navigation Bar */}
+      <nav className="flex items-center justify-between bg-gray-800 p-4">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="h-16 mr-4" />
+        </div>
+
+        {/* Logout Button */}
+        <button className="text-white font-impact">Logout</button>
+      </nav>
+
+      {/* Main Content */}
+      <div>
+        <center>
+        <h1 className="text-4xl mt-10 font-Helvetica">Start your cybersecurity learning</h1>
+        </center>
+        <div>
+        <center>
+        <h1 className="text-xl mt-5 font-Arial">Courses offered by Cyberpeace foundation</h1>
+        </center>
+
+        </div>
+      </div>
+      <div className="container mx-auto mt-8">
+        <div className="flex justify-center item-center gap-10">
+          {/* Card 1 */}
+          <div className="max-w-md bg-white shadow-md rounded-md overflow-hidden mr-4">
+            <img src="src/assets/image 4.png" alt="Placeholder" className="w-full h-64 object-cover" />
+            <div className="p-4">
+              <h2 className="text-xl font-bold mb-2">Computer Fundamentals</h2>
+              <p className="text-gray-700">course to get you ready with computers</p>
+            </div>
+          </div>
+
+          {/* Card 2 */}
+          <div className="max-w-md bg-white shadow-md rounded-md overflow-hidden">
+            <img src="src/assets/image 5.png" alt="Placeholder" className="w-full h-64 object-cover" />
+            <div className="p-4">
+              <h2 className="text-xl font-bold mb-2">CyberSecurity Fundamentals</h2>
+              <p className="text-gray-700">Start your journey in the field of cybersecurity</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
