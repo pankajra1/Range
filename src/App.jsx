a5e7cf69040ad9fbea4f05d969940a70ae03c2c6
pankajra1 <pankajrai_21911@aitpune.edu.in>
2024-02-13 22:22:33 +0530

    updated and fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,27 @@ import './App.css'
 
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+<<<<<<< HEAD
+import Home from './components/Home';
+import Footer from './components/Footer';
+function App() {
+    return (
+      <>
+      <Home/>
+     <Footer/>
+      </>
+=======
+import Home from "./Home"
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Login />} />
                 <Route path='/signup' element={<SignUp />} />
+                <Route path='/home' element={<Home />} />
             </Routes>
         </Router>
+>>>>>>> 73226ddc87a22f71ce2c8f327e9984db1c325840
     )
 }
 
