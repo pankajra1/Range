aa3b4e4cbd047e3f2844181558401fcd23a590c7
pankajra1 <pankajrai_21911@aitpune.edu.in>
2024-04-20 02:01:20 +0530

    Updated with new firebase, to integrate sso

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore, serverTimestamp } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD03UQJVf7mB3lXUVVsCAiO3JQW_5DqFzE",
-  authDomain: "range-b10ab.firebaseapp.com",
-  projectId: "range-b10ab",
-  storageBucket: "range-b10ab.appspot.com",
-  messagingSenderId: "744986694362",
-  appId: "1:744986694362:web:282fb7487d7434cd0bcbe3"
+  apiKey: "AIzaSyDPKPfsUhdsjlmE5ckn-FVpuqu_qes4ij8",
+  authDomain: "range-0000.firebaseapp.com",
+  projectId: "range-0000",
+  storageBucket: "range-0000.appspot.com",
+  messagingSenderId: "146426204838",
+  appId: "1:146426204838:web:4712b87720e6fd2bdf1a67",
+  measurementId: "G-RB5Z3QKQE5"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth  = getAuth(app);
+const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 export { auth, firestore, serverTimestamp };
 export default app;
