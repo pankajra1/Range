bc2579e5d95a399452d5feb3e59ba14e521bf07e
sauravkumarOm <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2024-04-23 02:11:51 +0530

    make navbar, content page responsive and add next and previous feature

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '/src/assets/logo.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGripLinesVertical, faUser } from '@fortawesome/free-solid-svg-icons'; // Added faUser for the profile icon.
+import React, { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "/src/assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faGripLinesVertical, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
+  const [userEmail, setUserEmail] = useState("");
   const dropdownRef = useRef(null);
+  const isMobile = useMediaQuery({ maxWidth: 598 });
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  // useEffect(() => {
+  //   function handleClickOutside(event) {
+  //     if (dropdownRef.current && !dropdownRef.current.contains(event.target) && event.target !== dropdownToggleRef.current) {
+  //       setDropdownOpen(false);
+  //     }
+  //   }
+
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, [dropdownRef]);
 
   useEffect(() => {
-    setUserEmail(sessionStorage.getItem('userEmail') || '');
+    setUserEmail(sessionStorage.getItem("userEmail") || "");
 
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -24,48 +44,109 @@ const Navbar = () => {
     };
   }, []);
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
+  const dropdownToggleRef = useRef(null);
 
   return (
-    <nav className="bg-gray-800 p-4 flex flex-col lg:flex-row lg:justify-between lg:items-center">
+    <nav className="bg-gray-800 p-4 flex justify-between items-center">
       {/* Logo */}
       <Link to="/home">
-        <img src={logo} alt="Logo" className="h-14 mb-4 lg:mb-0 lg:mr-4" />
+        <img src={logo} alt="Logo" className="h-10" />
       </Link>
 
       {/* Navigation Links */}
-      <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-4">
-        <Link to="/home" className="text-white font-bold text-xl lg:text-2xl hover:text-gray-300">Learn</Link>
-        <span className="text-white mx-2 hidden lg:inline">|</span>
-        <Link to="/Live" className="text-white font-bold text-xl lg:text-2xl hover:text-gray-300">Live</Link>
-        <span className="text-white mx-2 hidden lg:inline">|</span>
-        <Link to="/discussion" className="text-white font-bold text-xl lg:text-2xl hover:text-gray-300">Discussion</Link>
+      <div
+        className={`flex items-center space-x-4 ${
+          isMobile ? "hidden" : "block"
+        }`}
+      >
+        <Link to="/home" className="text-white hover:text-gray-300">
+          Learn
+        </Link>
+        <Link to="/Live" className="text-white hover:text-gray-300">
+          Live
+        </Link>
+        <Link to="/discussion" className="text-white hover:text-gray-300">
+          Discussion
+        </Link>
       </div>
 
       {/* Dropdown */}
-      <div className="relative mt-4 lg:mt-0">
-        <button onClick={toggleDropdown} className="text-white hover:text-gray-300 flex items-center">
-          <FontAwesomeIcon icon={faUser} className="h-6 mr-2" />
-          <span>{userEmail}</span>
-        </button>
+      <div className="relative z-20">
+        {isMobile && (
+          <>
+            <button
+              onClick={toggleDropdown}
+              className="text-white hover:text-gray-300 z-"
+              ref={dropdownToggleRef}
+            >
+              <FontAwesomeIcon icon={dropdownOpen ? faTimes : faBars} />
+            </button>
+          </>
+        )}
+        {!isMobile && (
+          <>
+            <button
+              onClick={toggleDropdown}
+              className="text-white hover:text-gray-300 flex items-center"
+            >
+              <FontAwesomeIcon icon={faUser} className="h-6 mr-2" />
+              <span>{userEmail}</span>
+            </button>
+          </>
+        )}
         {dropdownOpen && (
           <div
             ref={dropdownRef}
-            className="absolute bg-white mt-2 p-2 rounded shadow"
+            className="absolute bg-white mt-2 p-2 rounded shadow z-30"
             style={{
-              right: dropdownRef.current && dropdownRef.current.offsetWidth > 200 ? 'auto' : '0',
-              minWidth: '150px',
+              right:
+                dropdownRef.current && dropdownRef.current.offsetWidth > 200
+                  ? "auto"
+                  : "0",
+              minWidth: "150px", // Ensures the dropdown is wide enough
             }}
           >
-            <Link to="/profile" className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap">Profile</Link>
-            <Link to="/" className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap">Sign Out</Link>
+            {/* Conditionally render links based on isMobile */}
+            {isMobile && (
+              <>
+                <Link
+                  to="/home"
+                  className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap"
+                >
+                  Learn
+                </Link>
+                <Link
+                  to="/live"
+                  className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap"
+                >
+                  Live
+                </Link>
+                <Link
+                  to="/discussion"
+                  className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap"
+                >
+                  Discussion
+                </Link>
+              </>
+            )}
+            {!isMobile}
+            <Link
+              to="/profile"
+              className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap"
+            >
+              Profile
+            </Link>
+            <Link
+              to="/"
+              className="block text-gray-800 hover:bg-gray-200 py-1 px-4 whitespace-nowrap"
+            >
+              Sign Out
+            </Link>
           </div>
         )}
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
